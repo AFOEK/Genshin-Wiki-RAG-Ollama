432ff3cd3388cce318a5d45174376eb004843ea3
AFOEK <felix.montalfu@gmail.com>
2026-03-28 14:13:58 -0400

    Add filters

--- rag/utils/filters.py
+++ rag/utils/filters.py
@@ -1,9 +1,10 @@
 import re
 
 class Filters:
-    def __init__(self, deny_url_regex: str, deny_text_regex: str) -> None:
+    def __init__(self, deny_url_regex: str, deny_text_regex: str, allow_url_regex: str | None) -> None:
         self.deny_url = re.compile(deny_url_regex) if deny_url_regex else None
         self.deny_text = re.compile(deny_text_regex) if deny_text_regex else None
+        self.allow_url = re.compile(allow_url_regex) if allow_url_regex else None
 
     def url_allowed(self, url: str) -> bool:
         return not (self.deny_url and self.deny_url.search(url))
