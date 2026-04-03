eb340d6ad6799fef88632d988ae81ed82b28bf45
AFOEK <felix.montalfu@gmail.com>
2026-04-03 03:03:47 -0400

    Fix regex crash

--- rag/utils/filters.py
+++ rag/utils/filters.py
@@ -2,9 +2,9 @@ import re
 
 class Filters:
     def __init__(self, deny_url_regex: str, deny_text_regex: str, allow_url_regex: str | None = None) -> None:
-        self.deny_url = re.compile(deny_url_regex) if deny_url_regex else None
-        self.deny_text = re.compile(deny_text_regex) if deny_text_regex else None
-        self.allow_url = re.compile(allow_url_regex) if allow_url_regex else None
+        self.deny_url = re.compile(deny_url_regex, re.I) if deny_url_regex else None
+        self.deny_text = re.compile(deny_text_regex, re.I) if deny_text_regex else None
+        self.allow_url = re.compile(allow_url_regex, re.I) if allow_url_regex else None
 
     def url_allowed(self, url: str) -> bool:
         if self.deny_url and self.deny_url.search(url):
@@ -14,4 +14,4 @@ class Filters:
         return True
     
     def text_allowed(self, text: str) -> bool:
-        return not(self.deny_text and self.deny_text.search(text))
+        return not (self.deny_text and self.deny_text.search(text))
