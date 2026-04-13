2ae1a3f5e09c8fda7722cabaf7821aa28b5feb64
AFOEK <felix.montalfu@gmail.com>
2026-04-13 03:22:15 -0400

    Add new config and reranker scores

--- rag/qna/utils.py
+++ rag/qna/utils.py
@@ -12,8 +12,8 @@ log = logging.getLogger(__name__)
 
 INTENT_PROFILES = {
     "build": {
-        "source_bonus": {"kqm_tcl": 0.15, "game8_html": 0.10, "genshingg_html":0.08},
-        "source_penalty": {"genshin_wiki": 0.05, "kqm_news": 0.1},
+        "source_bonus": {"kqm_tcl": 0.15, "game8": 0.10, "genshin_gg":0.08},
+        "source_penalty": {"genshin_wiki": 0, "kqm_news": 0.1},
         "title_penalize": ["storyline", "voice", "voice-over", "story quest", "archon quest",
                            "world quest", "character card", "genius invokation",
                            "lore", "dialogue", "comments"],
@@ -27,7 +27,7 @@ INTENT_PROFILES = {
     },
     "lore": {
         "source_bonus": {"genshin_wiki": 0.1},
-        "source_penalty": {"honey": 0.08, "kqm_tcl": 0.05, "kqm_news": 0.03, "game8_html": 0.1, "genshingg_html":0.1},
+        "source_penalty": {"honey": 0.08, "kqm_tcl": 0.05, "kqm_news": 0.03, "game8": 0.1, "genshin_gg":0.1},
         "title_penalize": ["change history", "voice-overs", "character card",
                            "genius invokation", "normal attack", "constellation",
                            "ascension", "recommended", "signature weapon", "bis weapon",
@@ -40,7 +40,7 @@ INTENT_PROFILES = {
         "text_require_penalty": 0.0,
     },
     "mechanic":{
-        "source_bonus": {"genshin_wiki": 0.05, "kqm_tcl": 0.12, "game8_html": 0.07, "genshingg_html":0.06},
+        "source_bonus": {"genshin_wiki": 0.05, "kqm_tcl": 0.12, "game8": 0.07, "genshin_gg":0.06},
         "source_penalty": {"kqm_news": 0.05, "honey": 0.08},
         "title_penalize": ["storyline", "voice", "farewell", "wishes",
                            "character card", "story", "world quest", "archon quest", "character story", "comments"],
@@ -51,8 +51,60 @@ INTENT_PROFILES = {
         "text_require_any": [],
         "text_require_penalty": 0.0,
     },
+    "location": {
+        "source_bonus":   {"genshin_wiki": 0.20, "game8": 0.1},
+        "source_penalty": {"kqm_tcl": 0.1, "kqm_news": 0.08},
+        "title_penalize": [
+            "change history", "voice-overs", "character card", "genius invokation",
+            "normal attack", "constellation", "ascension", "comments",
+            "character story", "storyline",
+        ],
+        "title_boost": [
+            "region", "nation", "area", "domain", "dungeon", "cave",
+            "ruins", "city", "village", "mountain", "island", "forest",
+            "archon", "map", "exploration", "liyue", "mondstadt", "inazuma",
+            "sumeru", "fontaine", "natlan", "snezhnaya", "khaenri'ah", "nod-krai",
+            "isle", "enkanomiya", "sub-area", "sub-region", "the chasm", "chenyu vale", "dharma forest",
+            "great red sand", "girdle of the sands", "sea of bygone eras sea of bygone eras", "ancient sacred mountain",
+            "temple of space", "golden apple archipelago", "three realms gateway offering", "veluriyam mirage",
+            "simulanka", "dragonspine"
+        ],
+        "title_boost_v": 0.15,
+        "text_require_any": [
+            "located", "region", "nation", "area", "north", "south", "east", "west",
+            "near", "found in", "place", "city", "village", "ruins", "domain",
+            "map", "exploration", "coordinates", "waypoint", "liyue", "mondstadt", "inazuma",
+            "sumeru", "fontaine", "natlan", "snezhnaya", "khaenri'ah", "nod-krai", "enkanomiya", "the chasm",
+            "chenyu vale", "dharma forest", "great red sand", "girdle of the sands", 
+            "sea of bygone eras sea of bygone eras", "ancient sacred mountain",
+            "temple of space", "golden apple archipelago", "three realms gateway offering", "veluriyam mirage",
+            "simulanka", "dragonspine"
+        ],
+        "text_require_penalty": 0.20,
+    },
+
+    "biography": {
+        "source_bonus":   {"genshin_wiki": 0.15},
+        "source_penalty": {"kqm_tcl": 0.15, "honey": 0.05, "kqm_news": 0.05},
+        "title_penalize": [
+            "change history", "character card", "genius invokation",
+            "normal attack", "constellation", "ascension", "comments",
+            "voice-overs",
+        ],
+        "title_boost": [
+            "storyline", "character story", "lore", "profile",
+            "birthday", "background", "history",
+        ],
+        "title_boost_v": 0.20,
+        "text_require_any": [
+            "birthday", "born", "age", "from", "vision", "constellation",
+            "affiliation", "family", "related to", "sibling", "parent",
+            "friend", "title", "occupation", "voiced by",
+        ],
+        "text_require_penalty": 0.15,
+    },
     "general": {
-        "source_bonus":   {"genshin_wiki": 0.05},
+        "source_bonus":   {"genshin_wiki": 0.05, "game8":0.04},
         "source_penalty": {"kqm_news": 0.03},
         "title_penalize": ["character card", "genius invokation"],
         "title_boost":    [],
@@ -134,14 +186,34 @@ def detect_intent(question: str) -> str:
         "elemental skill", "elemental burst", "stamina", "scaling", "utility passive"
     ]
 
+    LOCATION_MARKERS = [
+    "where is", "where are", "where can i find", "location of",
+    "how to get to", "how do i get to", "map", "region", "nation",
+    "area", "domain", "dungeon", "explore", "exploration",
+    "directions", "waypoint", "teleport", "near", "coordinates",
+    "liyue", "mondstadt", "inazuma", "sumeru", "fontaine", "natlan", "enkanomiya"
+    ]
+
+    BIOGRAPHY_MARKERS = [
+        "birthday", "born", "age", "who is", "background of",
+        "related to", "family", "sibling", "parent", "friend of",
+        "relationship", "affiliated with", "affiliation", "vision holder",
+        "from where", "hometown", "nationality", "occupation", "bio",
+        "profile", "voiceline", "voiced by", "height",
+]
+
     build_hits = sum(1 for m in BUILD_MARKERS if m in q)
     lore_hits = sum(1 for m in LORE_MARKERS if m in q)
     mechanics_hits = sum(1 for m in MECHANICS_MARKERS if m in q)
+    location_hits = sum(1 for m in LOCATION_MARKERS if m in q)
+    biography_hits = sum(1 for m in BIOGRAPHY_MARKERS if m in q)
 
     scores = {
-        "build": build_hits,
-        "lore": lore_hits,
-        "mechanic": mechanics_hits
+        "build":     build_hits,
+        "lore":      lore_hits,
+        "mechanic":  mechanics_hits,
+        "location":  location_hits,
+        "biography": biography_hits,
     }
     best = max(scores, key=scores.get)
     if scores[best] == 0:
@@ -198,7 +270,7 @@ def rerank_chunks(question: str, chunks: list[dict], initial_scores: dict[int, f
         penalty = 0.0
         media_count = sum(text.count(ext) for ext in media_exts)
         if media_count > 3:
-            penalty += 0.20
+            penalty += 0.25
         if "character card" in title_l or "genius invokation" in title_l:
             penalty += 0.15
 
